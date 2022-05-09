e8a3e36137666adf451244943e31af7d776806f2
Dup4 <lyuzhi.pan@gmail.com>
2022-05-09 17:48:31 +0800

    fix: syntax error

--- release.sh
+++ release.sh
@@ -2,14 +2,14 @@
 
 TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-# if ! zstd -h >/dev/null 2>&1; then
-#     apt install zstd
-# fi
+if ! zstd -h >/dev/null 2>&1; then
+    apt install zstd
+fi
 
 TARGET_PATH="${TOP_DIR}/rootfs-packages"
 
 rm -rf "${TARGET_PATH}"
-mkdor "${TARGET_PATH}"
+mkdir "${TARGET_PATH}"
 
 for DIR in "${TOP_DIR}"/rootfs/*; do
     if [[ ! -d "${DIR}" ]]; then
