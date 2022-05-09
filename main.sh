ccaf4e27f52a4aa26b0dfbefc8148fa0be0ae362
Dup4 <lyuzhi.pan@gmail.com>
2022-05-09 22:26:25 +0800

    fix: copy rootfs dir

--- main.sh
+++ main.sh
@@ -32,8 +32,10 @@ docker run \
     --name="${RUNNER_NAME}" \
     "${IMAGE_NAME}"
 
+rm -rf "${TOP_DIR}"/rootfs
 rm -rf "${TOP_DIR}"/rootfs-packages
 
+docker cp -a "${RUNNER_NAME}":/root/rootfs "${TOP_DIR}"/
 docker cp -a "${RUNNER_NAME}:/root/rootfs-packages" "${TOP_DIR}"/
 docker rm -f "${RUNNER_NAME}"
 docker rmi "${IMAGE_NAME}"
